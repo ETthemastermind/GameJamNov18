3bc622e4bb4e9e8146214ac68dee29772a472059
Yovko Doynov <s5071741@bournemouth.ac.uk>
2018-11-08 17:28:05 +0000

    Movement script implemented

---
+++ GAME Jam 2018/Assets/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float JumpPower;
+	private bool OnGround=true; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (Vector3.right * MoveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (-Vector2.right * MoveSpeed * Time.deltaTime);
+		if (OnGround == true) 
+		{
+			if (Input.GetKey (KeyCode.W)) 
+			{
+				transform.Translate (Vector2.up * JumpPower);
+				OnGround = false;
+			}
+		}
+			
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Floor") 
+		{
+			OnGround = true;
+		}
+	}
+}
+
+
+
+
+
+
+
+
